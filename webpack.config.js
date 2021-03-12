bd5b178a9dc64bf89b9a8747bc90d23baf8b8ce5
Bruno Padilha <bpadilha.dev@gmail.com>
2021-03-11 23:46:09 -0300

    adiciona reactRefresh no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -16,18 +17,27 @@ module.exports = {
   },
   devServer: {
     contentBase: path.resolve(__dirname,'public'),
+    hot:true,
   },
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname,'public','index.html')
-    })
-  ],
+    }),
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /\.jsx$/, //will convert if the file is jsx
         exclude: /node_modules/, //will ignore all the files from node modules
-        use: 'babel-loader' //will use babel-loader to transpile every jsx file
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        } //will use babel-loader to transpile every jsx file
       },
       {
         test: /\.scss$/,
